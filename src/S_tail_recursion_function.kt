60b3f6dd1b9449ab8c1a3aa1753a9cb16d1d79f3
Sarvesh Patil <sarveshdpatil942@gmail.com>
2026-05-26 22:06:46 +0530

    learned tail recursive function

--- src/S_tail_recursion_function.kt
+++ src/S_tail_recursion_function.kt
@@ -1,3 +1,8 @@
+/*
+- tail recursion is same as normal recursion, it mostly don't store the function calls on the stack memory so it never get stack overflow error
+- in tail recursion while the function calls it-self it also perform the operation which was defined after the function call so it don't need to do that while returning
+
+ */
 fun factorial(num: Int, x: Int): Int {
     return if (num == 1) x
     else factorial(num-1, x*num)
