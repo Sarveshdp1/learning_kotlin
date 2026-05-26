dd1ca3e0ab7739003fac98103d50348fbf967d33
Sarvesh Patil <sarveshdpatil942@gmail.com>
2026-05-26 21:50:00 +0530

    learned anonymous function

--- src/U_anonymous_function.kt
+++ src/U_anonymous_function.kt
@@ -1,6 +1,7 @@
 // anonymous function is a function without name and store inside variable and call using variable name
 // anonymous function only return from it-self not the whole function
 
+// find number even or odd and negative or positive
 val evenOdd = fun (a: Int): String {
     return if ((a % 2 == 0) and (a > 0) ) "$a is even and positive"
     else if ((a % 2 != 0) and (a > 0)) "$a is odd and positive"
@@ -8,15 +9,24 @@ val evenOdd = fun (a: Int): String {
     else "$a is odd and negative"
 }
 
+/*
+- sum of 2 numbers
+- return as unit
+- explicitly datatype declaration
+ */
 var sum0: (Int,Int) -> Unit = fun(a,b): Unit {
     println("sum of $a + $b is ${a+b}")
 }
 
+// sum of 2 numbers and explicitly datatype declaration
 var sumf: (Int,Int) -> Int = {a, b -> a+b}
 
 fun main() {
+
+    // calling anonymous function and passing arguments
     println(evenOdd(-35))
     sum0(4,2)
+    sum1(5,20)
 
     val arr = arrayOf(1,2,3,4,5)
     // anonymous function return
