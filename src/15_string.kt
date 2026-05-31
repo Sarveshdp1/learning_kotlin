fdeec99d47501eba12abc915002984888c5e80c7
Sarvesh Patil <sarveshdpatil942@gmail.com>
2026-05-31 15:26:53 +0530

    kotlin learning files

--- src/15_string.kt
+++ src/15_string.kt
@@ -25,38 +25,42 @@ fun main(args: Array<String>) {
     println("elements from index 3 to 6 - ${myName.subSequence(3, 6)}") // element on end is not included excluded use +1 (3, 7)
 
     // String compareTo
-    var s1 = "Sarvesh"
-    var s2 = "Sarveshsghsg"
+    val s1 = "Sarvesh"
+    val s2 = "Sarveshsghsg"
     println("${s1.compareTo(s2)}")
 
     // types of string literals
     // 1. Escaped String = "hello" also called as single line string contain escape /n /t etc
     // 2. Raw String = """Hello""" also called as multi line string not contain escape
 
-    var escapeString: String = "Hello"
 
-    var rawString: String = """
+    // escaped string (single line string)
+    val escapeString: String = "Hello"
+    println(escapeString)
+
+    // raw string (multi line string)
+    val rawString: String = """
         |hello
         |my name
         |is 
         |sarvesh
     """.trimMargin()
+    println(rawString)
 
-    var rawString2: String = """
+    val rawString2: String = """
         my name is
         sarvesh
         hello how are
         you guys
     """.trimIndent()
-
     println(rawString2)
 
 
     // escape characters , \" \r \n \' \\ \t \b
 
-    var str1 = "Hello"
-    var str2 = "sarvesh"
-    var str3 = "Sarvesh"
+    val str1 = "Hello"
+    val str2 = "sarvesh"
+    val str3 = "Sarvesh"
 
     println(str2==str3) // == just check the values
     println(str1===str2) // === check the value and memory StringPool
@@ -65,41 +69,48 @@ fun main(args: Array<String>) {
         print("$i and $v\n")
     }
 
-    var string2: String = $$"""
+    // multi-dollar interpolation allowed to use multiple dollar sign to trigger the interpolation (string template)
+    // allows to use $ sign in string without any error
+    val string2: String = $$"""
         money $$$ $$ $${4+3}
     """.trimIndent()
 
-    println(string2)
-    var num = 40.09889094889749
-    var age:String = "Sarvesh"
-    var name1 = String.format("%s %S", age, age)
-    println(name1)
-    var formate2 = String.format("%f %.2f", num, num)
-    println(formate2)
-    var num1 = -484
-    var format3 = String.format("%(d mhanje %1\$d",num1)
-    println(format3)
 
+    // string formating
+    // useful to control the floating numbers (.000000 to .000 or .00/.0 anything)
+    println(string2)
+    val num = 40.09889094889749
+    val name:String = "Sarvesh"
+    val name1 = String.format("%s %S", name, name)
+    println(name1)
+
+    val formate2 = String.format("%f %.2f", num, num)
+    println(formate2)
+
+    val num1 = -484
+    val format3 = String.format("%(d mhanje %1\$d",num1)
+    println(format3)
 
     println("%(d %2\$d".format(-50, 399646466L))
 
-    myName = myName.replace("Sar", "Pgg")
-
-    println(myName)
-
-    var new = String.format("%s age is %d",myName, 555)
-
+    // created formated string variable
+    val new = String.format("%s age is %d",myName, 555)
     println(new)
 
+    // string functions
+    // allows to replace specific elements of string
+    myName = myName.replace("Sarv", "Gan") // Ganesh
+    println(myName.contains("sh")) // true Ganesh has sh
+    println(myName.startsWith("Gan")) // true Ganesh has Pur
+    println(myName.endsWith("sh")) // true Ganesh ends with sh
 
-    println(myName.contains("sh"))
+    // splits name with character available in string but default is , inside [] squire braces
+    val myNewName: String = "Sarvesh"
+    println(myNewName.split("")) //[,S,a,r,v,e,s,h]
 
-    println(myName.startsWith("Pgg"))
-
-    println(myName.endsWith("sh"))
-
-    var myNewName: String = "Sarvesh"
-
-    println(myNewName.split(""))
+    // trim, remove unwanted spaces from starting and ending of string
+    val greeting = "              Hello Everyone     How are you                         "
+    println(greeting)
+    println(greeting.trim())
 
 }
