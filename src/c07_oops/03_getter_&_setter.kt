631494ad026c4be742da6ea709822f4216568ac3
Sarvesh Patil <sarveshdpatil942@gmail.com>
2026-06-13 16:33:04 +0530

    kotlin learning files

--- src/c07_oops/03_getter_&_setter.kt
+++ src/c07_oops/03_getter_&_setter.kt
@@ -14,7 +14,7 @@ package c07_oops
 // 2. val - immutable (read-only) value cannot change after creation only getter works
 
 fun main() {
-    var name = ""
+    var name:String
     name = "Sarvesh"
     val age = 20
 
